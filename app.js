182f3db5c80423e76af8d31549fa739468d9b74f
kiastu <xlimitlessskeisx@gmail.com>
2015-01-31 17:18:14 -0500

    fix

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ if (env === 'production') {
 app.get('/', routes.index);
 
 // JSON API
-app.get('/api/name', api.name);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
